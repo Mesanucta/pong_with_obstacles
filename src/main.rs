e44994d94633620eb2cdb20a7068dd4e33438d19
Mesanucta <2848527149@qq.com>
2025-06-05 13:47:44 +0800

    窗口基本设置及调试插件

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{PresentMode, WindowTheme},
+    diagnostic::{FrameCount},
+};
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Pong With Obstacles".into(),
+                    name: Some("bevy.app".into()),
+                    resolution: (1280., 960.).into(),
+                    present_mode: PresentMode::AutoVsync,
+                    window_theme: Some(WindowTheme::Dark),
+                    resizable: false,
+                    enabled_buttons: bevy::window::EnabledButtons {
+                        maximize: false,
+                        ..Default::default()
+                    },
+                    visible: false,
+                    ..default()
+                }),
+                ..default()
+            }),
+        ))
+        .add_plugins(EguiPlugin { enable_multipass_for_primary_context: true })
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Update, make_visible)
+        .run();
+}
+
+fn make_visible(mut window: Single<&mut Window>, frames: Res<FrameCount>){
+    if frames.0 == 3{
+        window.visible = true;
+    }
 }
